b2347a5617ce6c6d3033bbc1dccdbb37669d1243
LeanLarroza <leandro.larroza@hotmail.com>
2020-09-26 21:25:55 +0200

    4 deploy

--- api/controllers/WarzoneController.js
+++ api/controllers/WarzoneController.js
@@ -8,6 +8,7 @@ exports.ready = function(req, res) {
 
 exports.getstats = function(req, res) {
 	API.login('leanlarroza@est.frba.utn.edu.ar', 'Larrydevc09').then(
+		console.log('Request: Player: ' + req.params.player + ' - Platform: ' + req.params.platform),
 		API.MWBattleData(req.params.player, req.params.platform)
 			.then((output) => {
 				res.json(output);
