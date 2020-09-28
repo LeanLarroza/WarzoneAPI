bceeab38286d1f6c4fbd85bf1c55cb61ac41954a
LeanLarroza <leandro.larroza@hotmail.com>
2020-09-28 17:10:12 +0200

    retry login

--- api/controllers/WarzoneController.js
+++ api/controllers/WarzoneController.js
@@ -14,7 +14,16 @@ exports.getstats = function(req, res) {
 				res.json(output);
 			})
 			.catch((err) => {
-				res.json({ message: req.params.player + ': ' + err });
+				API.login('leanlarroza@est.frba.utn.edu.ar', 'Larrydevc09').then(
+					console.log('Request: Player: ' + req.params.player + ' - Platform: ' + req.params.platform),
+					API.MWBattleData(req.params.player, req.params.platform)
+						.then((output) => {
+							res.json(output);
+						})
+						.catch((err) => {
+							res.json({ message: req.params.player + ': ' + err });
+						})
+				);
 			})
 	);
 };
