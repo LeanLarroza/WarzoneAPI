369b6194d8c5221a7d1a9ec0d566d3fc2e12724d
LeanLarroza <leandro.larroza@hotmail.com>
2020-09-28 18:15:06 +0200

    handle error

--- api/controllers/WarzoneController.js
+++ api/controllers/WarzoneController.js
@@ -8,16 +8,20 @@ exports.ready = function(req, res) {
 
 exports.getstats = function(req, res) {
 	console.log('Request: Player: ' + req.params.player + ' - Platform: ' + req.params.platform),
-		API.login('leanlarroza@est.frba.utn.edu.ar', 'Larrydevc09').then((res) => {
-			console.log(res);
-			if (API.isLoggedIn()) {
-				API.MWBattleData(req.params.player, req.params.platform)
-					.then((output) => {
-						res.json(output);
-					})
-					.catch((err) => {
-						res.json({ message: req.params.player + ': ' + err });
-					});
-			}
-		});
+		API.login('leanlarroza@est.frba.utn.edu.ar', 'Larrydevc09')
+			.then((res) => {
+				console.log('Result log-in: ' + res);
+				if (API.isLoggedIn()) {
+					API.MWBattleData(req.params.player, req.params.platform)
+						.then((output) => {
+							res.json(output);
+						})
+						.catch((err) => {
+							res.json({ message: req.params.player + ': ' + err });
+						});
+				}
+			})
+			.catch((err) => {
+				res.json({ message: req.params.player + ': ' + err });
+			});
 };
