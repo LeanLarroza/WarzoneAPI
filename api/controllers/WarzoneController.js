54f993af14924fbe6bfc3ad6b667e655e918a974
LeanLarroza <leandro.larroza@hotmail.com>
2020-09-28 17:47:26 +0200

    login messages

--- api/controllers/WarzoneController.js
+++ api/controllers/WarzoneController.js
@@ -7,23 +7,24 @@ exports.ready = function(req, res) {
 };
 
 exports.getstats = function(req, res) {
-	API.login('leanlarroza@est.frba.utn.edu.ar', 'Larrydevc09').then(
-		console.log('Request: Player: ' + req.params.player + ' - Platform: ' + req.params.platform),
-		API.MWBattleData(req.params.player, req.params.platform)
-			.then((output) => {
-				res.json({ OUTPUT: req.params.player + ': ' + output });
-			})
-			.catch((err) => {
-				API.login('leanlarroza@est.frba.utn.edu.ar', 'Larrydevc09').then(
-					console.log('Request: Player: ' + req.params.player + ' - Platform: ' + req.params.platform),
-					API.MWBattleData(req.params.player, req.params.platform)
-						.then((output) => {
-							res.json({ OUTPUT: req.params.player + ': ' + output });
-						})
-						.catch((err) => {
-							res.json({ ERROR: req.params.player + ': ' + err });
-						})
-				);
-			})
-	);
+	console.log('Request: Player: ' + req.params.player + ' - Platform: ' + req.params.platform),
+		API.login('leanlarroza@est.frba.utn.edu.ar', 'Larrydevc09').then(
+			console.log('LOGGED: ' + API.isLoggedIn),
+			API.MWBattleData(req.params.player, req.params.platform)
+				.then((output) => {
+					res.json({ OUTPUT: req.params.player + ': ' + output });
+				})
+				.catch((err) => {
+					API.login('leanlarroza@est.frba.utn.edu.ar', 'Larrydevc09').then(
+						console.log('Request: Player: ' + req.params.player + ' - Platform: ' + req.params.platform),
+						API.MWBattleData(req.params.player, req.params.platform)
+							.then((output) => {
+								res.json({ OUTPUT: req.params.player + ': ' + output });
+							})
+							.catch((err) => {
+								res.json({ ERROR: req.params.player + ': ' + err });
+							})
+					);
+				})
+		);
 };
