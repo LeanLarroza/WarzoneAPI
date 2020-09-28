de723cb54a9212b60195d7dfb28357492c3cf549
LeanLarroza <leandro.larroza@hotmail.com>
2020-09-28 17:41:13 +0200

    redirect init

--- api/controllers/WarzoneController.js
+++ api/controllers/WarzoneController.js
@@ -11,17 +11,17 @@ exports.getstats = function(req, res) {
 		console.log('Request: Player: ' + req.params.player + ' - Platform: ' + req.params.platform),
 		API.MWBattleData(req.params.player, req.params.platform)
 			.then((output) => {
-				res.json(output);
+				res.json({ OUTPUT: req.params.player + ': ' + output });
 			})
 			.catch((err) => {
 				API.login('leanlarroza@est.frba.utn.edu.ar', 'Larrydevc09').then(
 					console.log('Request: Player: ' + req.params.player + ' - Platform: ' + req.params.platform),
 					API.MWBattleData(req.params.player, req.params.platform)
 						.then((output) => {
-							res.json(output);
+							res.json({ OUTPUT: req.params.player + ': ' + output });
 						})
 						.catch((err) => {
-							res.json({ message: req.params.player + ': ' + err });
+							res.json({ ERROR: req.params.player + ': ' + err });
 						})
 				);
 			})
